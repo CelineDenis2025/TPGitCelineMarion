d527ece0063a9077189db104c0deb93086383fe8
celine.denis <celine.denis29@yahoo.fr>
2026-01-07 11:55:51 +0100

    First commit creer-utilisateur

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 import application.domaine.Tache;
 import application.domaine.Utilisateur;
+import application.repositories.UtilisateurRepository;
 import application.services.TacheService;
 import application.services.UtilisateurService;
 
 void main() {
-	List<Utilisateur> listeUtilisateurs = new ArrayList<>();
-	List<Tache> listeTaches = new ArrayList<>();
+	UtilisateurRepository utilisateurRepository = new UtilisateurRepository();
 
 	TacheService taches = new TacheService();
 	UtilisateurService utilisateurs = new UtilisateurService();
 
 
 	//Placez vos appels de méthodes ici
+
+	// Test créer un utilisateur
+	// utilisateurs.creerUtilisateur("Paulin", "Novotny");
+	IO.println(utilisateurRepository.getUtilisateurs());
 }
--- src/application/services/UtilisateurService.java
+++ src/application/services/UtilisateurService.java
@@ -1,5 +1,6 @@
 package application.services;
 
+import application.domaine.Utilisateur;
 import application.repositories.UtilisateurRepository;
 
 public class UtilisateurService {
@@ -9,4 +10,17 @@ public class UtilisateurService {
 	public UtilisateurService() {
 		utilisateurRepository = new UtilisateurRepository();
 	}
+
+
+	// Méthode créer un utilisateur
+	public void creerUtilisateur(String prenom, String nom) {
+
+		String identifiant = prenom.substring(0).toLowerCase() + nom.toLowerCase();
+		String courriel = prenom + "." + nom + "@mail.com";
+		Utilisateur utilisateur = new Utilisateur(identifiant, nom, prenom, courriel);
+		utilisateurRepository.getUtilisateurs().add(utilisateur);
+
+	}
 }
+
+
